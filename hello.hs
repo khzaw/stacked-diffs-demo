61da754fbaed30af996a512f32f81bc99eca6b3c
Kaung Htet <kayhzaw@gmail.com>
2024-11-13 00:00:43 +0800

    Add basic list operation

--- hello.hs
+++ hello.hs
@@ -10,3 +10,7 @@ farewell name = "Goodbye, " ++ name ++ "!"
 -- Addition 1: Simple boolean check
 isShortName :: String -> Bool
 isShortName name = length name < 5
+
+-- Addition 2: Basic list operation
+repeatName :: String -> Int -> [String]
+repeatName name times = replicate times name
