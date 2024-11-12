134bb67ad7ffefb4c8b7d5084d4402d7bf07977a
Kaung Htet <kayhzaw@gmail.com>
2024-11-13 00:01:19 +0800

    Simplify type definition and header

--- hello.hs
+++ hello.hs
@@ -10,3 +10,13 @@ farewell name = "Goodbye, " ++ name ++ "!"
 -- Addition 1: Simple boolean check
 isShortName :: String -> Bool
 isShortName name = length name < 5
+
+-- Addition 2: Basic list operation
+repeatName :: String -> Int -> [String]
+repeatName name times = replicate times name
+
+-- Addition 3: Simple type definition and helper
+data Greeting = Hello | Goodbye
+formatGreeting :: Greeting -> String
+formatGreeting Hello = "Hello!"
+formatGreeting Goodbye = "Bye!"
